da279f2ebc88a3c6df9d542185749070542c78f2
Sana777-art <sanaranicrm22@gmail.com>
2026-02-28 07:32:04 +0000

    UPDATE 01BASIC

---
+++ per.js
@@ -0,0 +1,27 @@
+let basicsalary= 60000;
+let bonus = 10000;
+let tax = 5000;
+let amount=basicsalary+bonus;
+let total=amount-tax; 
+
+console.log ("basicsalary:", basicsalary);
+console.log ( "bouns:", bonus);
+console.log ("tax:", tax);
+console.log ("total salry:", totalsalary);
+
+let totalsalary=basicsalary+bonus-tax;
+
+let StudentName= "Ali";
+let courseName= "web development";
+let coursefee=30500;
+let discount= 500;
+
+let totalfee=coursefee-discount;
+
+console.log ("student name:", studentName);
+console.log ("coursename:", courseName);
+console.log ("coursefee:",coursefee );
+console.log ("discount:",discount);
+console.log("totalfee:",totalfee); 
+
+
