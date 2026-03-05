ffc4039f8cccf72096c9569ed648ba4df13e347d
Sana777-art <sanaranicrm22@gmail.com>
2026-03-05 05:55:33 +0000

    update 1basic.js

---
+++ 01basic.js
@@ -0,0 +1,51 @@
+let studentName= "Ali";
+let totalMarks =500; 
+let obtainedMarks = 425;
+
+let percentage = (obtainedMarks/ totalMarks)* 100;
+
+console.log("student:",studentName);
+console.log ("Total Marks:", totalMarks);
+console.log ("Obtained Marks:", obtainedMarks );
+console.log ( "Percentage:", percentage+"%");
+
+let age = 24 ;
+let  currentyear = 2026;
+let birthyear=currentyear-age;
+
+ 
+console.log ("age:", age);
+console.log ("currentyear:", currentyear);
+console.log ( "birthyear:", birthyear);
+
+
+let basicsalary= 60000;
+let bonus = 10000;
+let tax = 5000;
+let amount=basicsalary+bonus;
+let total = amount-tax; 
+let totalsalary=basicsalary+bonus-tax;
+console.log ("basicsalary:", basicsalary);
+console.log ( "bouns:", bonus);
+console.log ("tax:", tax);
+console.log ("total salry:", totalsalary);
+
+// 
+
+// let StudentName= "Ali";
+// let courseName= "web development";
+// let coursefee=30500;
+// let discount= 500;
+
+// let totalfee=coursefee-discount;
+
+// console.log ("student name:", studentName);
+// console.log ("coursename:", courseName);
+// console.log ("coursefee:",coursefee );
+// console.log ("discount:",discount);
+// console.log("totalfee:",totalfee); 
+
+
+
+
+
---
+++ 1basic.js
@@ -0,0 +1,27 @@
+
+let Nameofstudent="Ali";
+let fathername= "Aman";
+let Rollnumber=11;
+
+console.log("Nameofstudent:"+Nameofstudent,"fathername:"+fathername, "Rollnumber:"+Rollnumber);
+
+
+let English= 65;
+let Islamiyat =98;
+let biology= 60;
+let chemistry= 60;
+let physics=59;
+
+let totalmark=English+Islamiyat+biology+chemistry+physics;
+let percentage=(totalmark/1000)*100;
+
+console.log("English:"+English);
+console.log("Islamiyat:"+Islamiyat);
+console.log("biology:"+biology);
+console.log("chemistry:"+ chemistry);
+console.log("physics:"+physics);
+console.log( "totalmark:"+totalmark );
+console.log("percentage"+percentage+"%");
+console.table (["Nameofstudent:"+Nameofstudent,"fathername:"+fathername,"Rollnumber:"+Rollnumber,"English:"+English,"Physics:"+physics,"Islamiyat:"+Islamiyat,"Biology:"+biology,"chemistry:"+chemistry]);
+
+
