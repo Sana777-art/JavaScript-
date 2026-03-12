85dd1fd53b54455b2ae7edb40636a1dcb62e1978
Sana777-art <sanaranicrm22@gmail.com>
2026-03-12 05:44:53 +0000

    test.js

--- test.js
+++ test.js
@@ -1,6 +1,6 @@
 console.log("hello/ \\\\\/\\\nworld/\\")
 console.log(2+2)
-let name = {"name": "ali" ,"age" :21,"class":"python"   ,"Gender" :"M"}
+let name = {"name": "Ali" ,"age" :21,"class":"Python","Gender" :"M"}
 // const_Name=name;
 
 console.table(name)
