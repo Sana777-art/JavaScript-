11fc808393b7a5852525fbda24aa492b030e33b0
Sana777-art <sanaranicrm22@gmail.com>
2026-03-26 08:49:03 +0000

    Array.js

--- Array.js
+++ Array.js
@@ -1,1 +1,24 @@
-const car
+const car =[ 0,1,2,3,4,5]
+console.log (car)
+const myarr = new Array (1,2,3,4,5)
+console.log (myarr)
+myarr. push (6)
+console.log(myarr)
+const cars=["bmw","alto","volvo"];
+console.log(cars)
+myarr.push (7)
+console.log (myarr)
+myarr.pop (0)
+myarr.unshift (0)
+console.log (myarr)
+myarr.shift (5)
+console .log (myarr.includes (9))
+console.log(myarr.indexOf (3))
+//slice.space
+console .log("Array",myarr) 
+const arr= myarr.slice(1,4)
+console.log("B".arr)
+const arr2=myarr.slice(1,4)
+console.log(arr2)
+console.log (car,myarr)
+
