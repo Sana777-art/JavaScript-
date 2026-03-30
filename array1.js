6e2a8833638ebc6a82d7a055499ccb62c39d4ab3
Sana777-art <sanaranicrm22@gmail.com>
2026-03-30 08:29:33 +0000

    array1

---
+++ array1.js
@@ -0,0 +1,27 @@
+const student =["Sawera","Eman","mahek","Amna"]
+const newstudent = ["Naila","Hira","Saba" ]
+student.push (newstudent);
+console.log (student); 
+student.concat(newstudent);
+console.log(newstudent);
+const num = [[1,2,3],[4,5,6],[7,8,9]]
+console.log =[num [0][2][0]]
+
+const teacher=["jamela","Sara","Iqra"]
+const students=["Naila","Hira","Saba"]
+teacher.find [student];
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
