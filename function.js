3eebde8b1d3c871d62da973fba66a4f18dcf48b5
Sana777-art <sanaranicrm22@gmail.com>
2026-04-06 08:41:18 +0000

    function.js

---
+++ function.js
@@ -0,0 +1,37 @@
+function sayname(){
+  console.log ("S");
+  console.log ("a");
+  console.log ("n");
+  console.log ("a");
+  
+}
+
+ function name (){
+    console.log("helllo");
+    
+ }
+  sayname()
+    
+function person (Name,Batch){
+   return  `Name____${Name}
+   batch____${Batch} 
+   `
+   
+
+}
+
+let sdt1= person("Ali","python")
+let sdt2= person("sana","python")
+
+console.log (sdt1)
+console.log(sdt2)
+
+//Function Parameters
+
+function sub(num1,num2){
+    return num1-num2;
+
+
+}
+su=sub(12,4)
+console.log (su)
