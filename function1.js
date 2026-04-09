221a84d6dcafd8a8f77d57edda859ee525f2c704
Sana777-art <sanaranicrm22@gmail.com>
2026-04-09 07:35:57 +0000

    task2.js

---
+++ function1.js
@@ -0,0 +1,66 @@
+let name="ali"
+if(name=="ali"){
+ console.log(`welcom ${name}`)
+ 
+}
+
+ else
+    {console.log ("invalid")
+
+ }
+
+   
+
+function sum(num1,op,num2){
+    if (op==="+"){
+     return num1+num2   
+    }
+
+}   
+   
+let cal=sum(10,"+",40)
+console.log (cal)
+
+      //calculator add
+function sub(num1,op,num2){
+    if (op==="-"){
+        return  num1-num2
+    
+    }
+    else if (op === "-"){
+        return num1-num2
+    }
+
+}
+ console.log(sub
+ (50,"-",20) )
+
+  
+
+function mul(num1,op,num2){
+    if (op==="*"){
+        return  num1-num2
+    
+    }
+    else if (op === "*"){
+        return num1-num2
+    }
+
+}
+ console.log(mul
+ (50,"*",60) )
+
+
+
+function div(num1,op,num2){
+    if (op==="/"){
+        return  num1-num2
+    
+    }
+    else if (op === "/"){
+        return num1-num2
+    }
+
+}
+ console.log(div
+ (60,"/",20) )
---
+++ task2.js
@@ -0,0 +1,49 @@
+function student (rollno){
+    if (rollno===201){
+        return`
+        Rollno.______${201}
+        Name.______${"Ali"}
+        Class._____${12}
+        `      
+        
+    }else if(rollno===202){
+      return`
+      Rollno.______${202}
+      Name._______${"sara"}
+      Class.______${12}
+      `
+
+    }else if(rollno===203){
+      return`
+      Rollno.______${203}
+      Name._______${"Hina"}
+      Class.______${12}
+      `
+
+    }else if(rollno===204){
+      return`
+      Rollno.______${204}
+      Name._______${"Saba"}
+      Class.______${12}
+      `
+
+    } else if(rollno===205){
+      return`
+      Rollno.______${205}
+      Name._______${"Iqra"}
+      Class.______${12}
+      `
+
+    }else if(rollno===206){
+      return`
+      Rollno.______${206}
+      Name._______${"Dua"}
+      Class.______${12}
+      `
+
+    }
+    
+    
+
+}
+console.log (student(205))
