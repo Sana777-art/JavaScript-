09610eed80ea7920df6c830be061bd1cedc51296
Sana777-art <sanaranicrm22@gmail.com>
2026-04-09 08:30:39 +0000

    function.2js

---
+++ function2.js
@@ -0,0 +1,20 @@
+function user(username,password){
+    if(username==="dua"){
+        if (password===123){  
+    
+        return`welcom to $ {dua}`
+
+        
+    }
+    else{
+        return"invaild password";
+
+    }
+    }
+else{
+ 
+    return "username and password invaild" 
+    
+}
+}
+console.log (user(123))
--- task.js
+++ task.js
@@ -2,4 +2,4 @@ let side1 =5;
 let side2 =6;
 let side3 = 7;
 let s = (side1+ side2+ side)/2;
-let 
+let area = Math.sqrt() 
